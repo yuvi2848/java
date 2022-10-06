b71287c948a364495598981e2d91846673a45946
yuvi2848 <98656223+yuvi2848@users.noreply.github.com>
2022-10-06 19:48:43 +0530

    Create input.java
    taking input from the user

---
+++ input.java
@@ -0,0 +1,19 @@
+import javax.naming.Name;
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args){
+        //input
+       /*Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);*/
+
+
+        // Question
+        //Take two variable a and b and print their sum
+
+       /* Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);*/
