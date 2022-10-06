2e78ea20c02b5c168c4d799ee5f96eded48dc51d
yuvi2848 <98656223+yuvi2848@users.noreply.github.com>
2022-10-06 19:53:31 +0530

    Create patternquestions.java

---
+++ patternquestions.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class patternquestion {
+    public static void main(String args []){
+        // print the pattern
+        //****
+        // ****
+        // ****
+        // ****
+       // System.out.println("****");
+       // System.out.println("****");
+        //System.out.println("****");
+       // System.out.println("****");
+       // another way to solve this for loop
+    /*  int n = 4;
+      int m = 5;
+       //outer loop or neated loop
+        for(int i=1; i<=n; i++){
+            // inner loop
+            for(int j=1; j<=m; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+
+        //print the pattern hollow rectangle
+
+    }
+}
