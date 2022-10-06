8a3c71b8f7b1b70063a8f11bef884576b7cd4a95
yuvi2848 <98656223+yuvi2848@users.noreply.github.com>
2022-10-06 19:55:01 +0530

    Create numbers.java

---
+++ numbers.java
@@ -0,0 +1,12 @@
+public class Number {
+    public static void main(String[] args) {
+        //variable
+        int a = 10;
+        int b = 25;
+        int sum = a + b;
+       // System.out.println(sum);
+        int diff = b - a;
+        System.out.println(diff);
+
+    }
+}
