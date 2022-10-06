754776d0522e79b9caab2a0c8f5860b72e568fbd
yuvi2848 <98656223+yuvi2848@users.noreply.github.com>
2022-10-06 19:49:27 +0530

    Create loops.java
    loops

---
+++ loops.java
@@ -0,0 +1,13 @@
+public class loop {
+    public static void main(String[] args){
+        // couter++
+      // for(int counter = 0; counter < 11; counter++){
+         //  System.out.println(counter);
+      // }
+        // print the number from 0 to 10
+        //for int i = 0; i<11; i++
+        for(int i = 0; i < 11; i++){
+            System.out.println(i+"");
+        }
+    }
+}
